554ff7dfb0da5a5f02761439ff7ff832dd72b1de
YosuaTriantara <yosuatriantara@gmail.com>
2025-12-24 01:30:49 +0800

    fix .\requirements.txt

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,9 +7,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 def train(data_file, n_estimators):
-    print("=== Memulai Training Otomatis via MLflow Project ===")
+    print("Memulai Training Otomatis via MLflow Project")
     
-    # 1. Load Data
+    #Load Data
     try:
         df = pd.read_csv(data_file)
     except Exception as e:
@@ -20,21 +20,18 @@ def train(data_file, n_estimators):
     y = df['Target_Churn']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # 2. Setup MLflow
+    #Setup MLflow
     mlflow.set_tracking_uri("./mlruns") 
     mlflow.set_experiment("CI_CD_Churn_Experiment")
 
-    # 3. Training & Logging
+    #Training & Logging
     with mlflow.start_run() as run:
-        # Log Parameter
         mlflow.log_param("n_estimators", n_estimators)
         
-        # Train Model
         print(f"Training RandomForest dengan n_estimators={n_estimators}...")
         model = RandomForestClassifier(n_estimators=n_estimators, random_state=42)
         model.fit(X_train, y_train)
         
-        # Evaluate
         acc = accuracy_score(y_test, model.predict(X_test))
         mlflow.log_metric("accuracy", acc)
         print(f"Akurasi: {acc}")
